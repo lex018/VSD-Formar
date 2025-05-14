31d1b6af2ebe34932848fde3c4fb9f63a2aa01aa
Alexzy <chinelatojacqueline@gmail.com>
2025-05-14 00:04:28 +0000

    criação de usuário

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import express from "express";
+import { mainModule } from "process";
+import {sequelize} from "./config/database";
+import User from "./models/User";
 
 const app = express();
 const port = 3000;
@@ -7,4 +10,19 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
+main()
+
+
+async function main() {
+  if (require.main === module) {
+    await sequelize.sync({ force: true }).then(() => {
+      console.log("Database is synced");
+      app.listen(port, () =>
+        console.log(`Listening at http://localhost:${port}`),
+    );
+    });
+
+    const user = await User.create({ name: 'Alice', email: 'alice@email.com'});
+    console.log("Usuário criado:", user.toJSON());
+  }
+}
